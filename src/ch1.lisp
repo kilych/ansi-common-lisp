eec44f7105dda901f51cedbfdbee3f7b7a8acb08
kilych <kilych@zoho.com>
2021-07-21 04:52:33 +0300

    Initial commit

---
+++ src/ch1.lisp
@@ -0,0 +1,22 @@
+;; 1.1
+
+(defun sum (n)
+  (let ((s 0))
+    (dotimes (i n s)
+      (incf s i))))
+
+(= (sum 5) 10)
+
+(defun addn (n)
+  #'(lambda (x) (+ x n)))
+
+(funcall (addn 4) 2)
+
+;; optional
+
+(defun addn-times (n times)
+  (let ((s 0))
+    (dotimes (i times s)
+      (setf s (funcall (addn n) s)))))
+
+(addn-times 2 10)
---
+++ src/ch2.lisp
@@ -0,0 +1,167 @@
+;; 2.4
+
+(third '(a b c d))
+
+;; 2.5
+
+(listp '(1 2 3))
+
+(null nil)
+
+(not nil)
+
+;; Special operators (different order of evaluation): if
+
+(if (listp '(1 2 3))
+    (+ 1 2)
+    (+ 5 6))
+
+(if (listp 27)
+    (+ 1 2)
+    (+ 5 6))
+
+;; Macros: and, or
+
+(and t (+ 1 2))
+
+(or nil 27)
+
+;; 2.6
+
+(defun our-third (lst)
+  (car (cdr (cdr lst))))
+
+(> (+ 1 4) 3)
+;; |
+;; V
+(defun sum-greater (x y z)
+  (> (+ x y) z))
+
+(sum-greater 1 4 3)
+
+;; 2.7
+
+(defun our-member (obj lst)
+  (if (null lst)
+      nil
+      (if (eql (car lst) obj)
+          lst
+          (our-member obj (cdr lst)))))
+
+(our-member 2 '(1 3 2 a 4))
+
+;; 2.9
+
+(format t "~A plus ~A equals ~A.~%" 2 3 (+ 2 3))
+
+(defun askem (string)
+  (format t "~A" string)
+  (read))
+
+;; Seems to be broken. Different behavior: lispy "e" vs C-x C-e
+;; Works when called from repl.
+(askem "How old are you? ")
+
+;; 2.10
+
+(let ((x 1) (y 2))
+  (+ x y))
+
+(defun ask-number ()
+  (format t "Please enter a number. ")
+  (let ((val (read)))
+    (if (numberp val)
+        val
+        (ask-number))))
+
+(defparameter *glob* 99)
+
+(defconstant +limit+ (+ *glob* 1))
+
+(boundp '*glob*)
+
+;; 2.11
+
+(setf *glob* 98)
+
+(let ((n 10))
+  (setf n 2)
+  n)
+
+;; implicit declaration
+(setf x (list 'a 'b 'c))
+
+(setf (car x) 'n)
+
+(setf a 'b
+      c 'd
+      e 'f)
+
+;; 2.12
+
+(setf lst '(c a r a t))
+
+(remove 'a lst)                         ; immutable
+
+(setf x '(c a r a t))
+
+(setf x (remove 'a x))
+
+;; 2.13
+
+(defun show-squares (start end)
+  (do ((i start (+ i 1)))
+      ((> i end) 'done)
+    (format t "~A ~A~%" i (* i i))))
+
+(show-squares 2 5)
+
+(defun show-squares (start end)
+  (if (> start end)
+      'done
+      (progn
+        (format t "~A ~A~%" start (* start start))
+        (show-squares (+ start 1) end))))
+
+(show-squares 2 5)
+
+(defun our-length (lst)
+  (let ((len 0))
+    (dolist (obj lst)
+      (setf len (+ len 1)))
+    len))
+
+(our-length '(a b c))
+
+(defun our-length (lst)
+  (if (null lst)
+      0
+      (+ (our-length (cdr lst)) 1)))
+
+(our-length '(a b c))
+
+;; 2.14
+
+(function +)
+
+(apply (function +) '(1 2 3))
+
+(apply #'+ '(1 2 3))
+
+(+ 1 2 3)
+
+(apply #'+ 1 2 '(3 4 5))
+
+(funcall #'+ 1 2 3)
+
+(lambda (x y)
+  (+ x y))
+
+((lambda (x) (+ x 100)) 1)
+
+(funcall #'(lambda (x) (+ x 100))
+         1)
+
+;; 2.15
+
+(typep 27 'integer)
